ec90005f4caa6604484fcc590373d1dce665d393
saikrishna <saikrishna.capg@gmail.com>
2020-05-15 20:51:18 +1000

    Changes made to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+DIR=$HOME/gradle-6.4
 cd $HOME 
-curl -k https://downloads.gradle-dn.com/distributions/gradle-6.4-bin.zip -o gradle.zip
-unzip gradle.zip
+if [ ! -d "$DIR" ]; then
+  echo "Installing gradle files in ${DIR}..."
+  curl -k https://downloads.gradle-dn.com/distributions/gradle-6.4-bin.zip -o gradle.zip
+  unzip gradle.zip
+fi
 
-export PATH=$PATH:$HOME/gradle-6.4/bin
-export GRADLE_HOME=$HOME/gradle-6.4
+if ! grep -q "GRADLE_HOME" ~/.bashrc; then
+   export PATH=$HOME/gradle-6.4/bin:$PATH
+   export GRADLE_HOME=$HOME/gradle-6.4 
+   . ~/.bashrc
+fi
+cd -
+echo $PATH
+pwd
 
 rm -rf build
-gradle build
+$HOME/gradle-6.4/bin/gradle build
 
 docker-compose up
